ee7ac63b2269e321e3fcaaa3f63c66491b662f79
wexx <wex@stu.ouc.edu.cn>
2023-01-16 21:06:58 +0800

    refactor: author

--- src/components/in-page/Post/aside/author.tsx
+++ src/components/in-page/Post/aside/author.tsx
@@ -1,10 +1,10 @@
-import { Author as PostAuthor } from '~/components/universal/Author'
+import { Author } from '~/components/universal/Author'
 import { Card } from '~/components/universal/Card'
 
-export const Author = () => {
+export const PostAuthor = () => {
   return (
     <Card>
-      <PostAuthor
+      <Author
         avatar="https://p1-jj.byteimg.com/tos-cn-i-t2oaga2asx/mirror-assets/168e0858b6ccfd57fe5~tplv-t2oaga2asx-no-mark:100:100:100:100.awebp"
         name="掘金酱"
         intro="❤首席客服君 @ 掘金"
