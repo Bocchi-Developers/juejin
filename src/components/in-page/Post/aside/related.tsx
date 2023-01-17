9d54936f2e2b1d454480ad356e240080adffa95f
suemor <3100825062@qq.com>
2023-01-17 22:59:24 +0800

    chore: style

--- src/components/in-page/Post/aside/related.tsx
+++ src/components/in-page/Post/aside/related.tsx
@@ -1,31 +1,37 @@
+import Link from 'next/link'
+
 import { Card } from '~/components/universal/Card'
 
 import style from './related.module.less'
 
-const headerStyle = {
-  padding: '1.333rem 0',
-  margin: '0 1.667rem',
-  'font-size': '16px',
-  'line-height': '2rem',
-  height: '57px',
-}
+const Post = [
+  {
+    title: '🏆掘金年度征文 | 2021年终总结征文大赛',
+    id: '1',
+  },
+  {
+    title: '做了一份前端面试复习计划，保熟～',
+    id: '2',
+  },
+  {
+    title: ' 10月更文诚意加码，激发写作潜力｜掘金·日新计划',
+    id: '3',
+  },
+  {
+    title: '掘金日新计划 ｜ 12月更文挑战来袭，开启掘金成长之旅',
+    id: '4',
+  },
+]
 
 export const RelatedPost = () => {
   return (
-    <Card
-      title={'相关文章'}
-      headerStyle={headerStyle}
-      bodyStyle={{ padding: '16px 0' }}
-    >
-      <div className={style.container}>
-        <div>🏆掘金年度征文 | 2021年终总结征文大赛 194点赞 · 597评论</div>
-        <div>🏆掘金年度征文 | 2021年终总结征文大赛 194点赞 · 597评论</div>
-        <div>🏆掘金年度征文 | 2021年终总结征文大赛 194点赞 · 597评论</div>
-        <div>🏆掘金年度征文 | 2021年终总结征文大赛 194点赞 · 597评论</div>
-        <div>🏆掘金年度征文 | 2021年终总结征文大赛 194点赞 · 597评论</div>
-        <div>🏆掘金年度征文 | 2021年终总结征文大赛 194点赞 · 597评论</div>
-        <div>🏆掘金年度征文 | 2021年终总结征文大赛 194点赞 · 597评论</div>
-        <div>🏆掘金年度征文 | 2021年终总结征文大赛 194点赞 · 597评论</div>
+    <Card title={'相关文章'}>
+      <div className={style.list}>
+        {Post.map(({ title, id }) => (
+          <Link key={id} href={id}>
+            {title}
+          </Link>
+        ))}
       </div>
     </Card>
   )
