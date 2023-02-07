7845a64053495f4c1625c6028c16fc5256856ff4
suemor <3100825062@qq.com>
2023-02-07 09:43:34 +0800

    feat: home getInitialProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,32 @@
 import type { NextPage } from 'next'
+import { createContext } from 'react'
 
 import { Advertisement } from '~/components/in-page/Home/aside/advertisement'
 import { AuthorRecommend } from '~/components/in-page/Home/aside/author'
 import { List } from '~/components/in-page/Home/list'
 import { Navbar } from '~/components/in-page/Home/navbar'
 import { ArticleLayout } from '~/components/layouts/ArticleLayout'
+import { AggregateApi } from '~/services/api/aggregate'
+import type { IAggregateHome } from '~/types/api/aggregate'
 
 const sidebar = [Advertisement, AuthorRecommend]
 
-const Home: NextPage = () => {
+const Home: NextPage<IAggregateHome> = (props) => {
   return (
-    <>
+    <HomeSidebarContext.Provider value={props}>
       <Navbar />
       <ArticleLayout aside={sidebar} asideWidth={240}>
         <List />
       </ArticleLayout>
-    </>
+    </HomeSidebarContext.Provider>
   )
 }
+
+Home.getInitialProps = async () => {
+  const aggregate = await AggregateApi.aggregateHomeRequest(3)
+  return aggregate
+}
+
+export const HomeSidebarContext = createContext<IAggregateHome | null>(null)
 
 export default Home
